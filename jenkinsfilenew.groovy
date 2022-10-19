1cdb2350a50bffe3842fa06ce8501eca23265872
mahesh <maheshryali@gmail.com>
2022-10-19 13:10:37 +0530

    changes1

--- jenkinsfilenew.groovy
+++ jenkinsfilenew.groovy
@@ -1,16 +1,24 @@
 pipeline {
     agent { label 'SHOPIZER'}
-    triggers { cron '30 7 * * *'}
+    triggers { cron '42 7 * * *'}
     stages{
         stage('vcs') {
             steps {
-            git branch:'release',
+            git branch:'promote',
                    url: 'https://github.com/maheshryali/shopizer.git'
             }
         }
         stage('maven') {
             steps {
                 sh 'mvn package'
+            }
+        }
+        stage('merge') {
+            steps {
+                sh """
+                git checkout release
+                git merge develop --no-ff
+                """
             }
         }
     }
